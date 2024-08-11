e720747d9cf66ba6246847294610f7e22177ec05
Oriol Fernandez Peña <oriol.fdez.penya@gmail.com>
2024-08-11 12:34:22 +0200

    Distances

---
+++ higherdimknn/distances/distances.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+def minkowski_dist(x: np.ndarray, y: np.ndarray, p: float):
+    if p <= 0:
+        return ValueError("p must be > 0")
+    
+    _dif = np.abs(x.flatten() - y.flatten())
+    return np.power(np.power(_dif, p).sum(), 1/p)
+
+def euclidean_dist(x: np.ndarray, y: np.ndarray):
+    return minkowski_dist(x, y, 2)
+
+def manhattan_dist(x: np.ndarray, y: np.ndarray):
+    return minkowski_dist(x, y, 1)
