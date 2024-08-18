9cb9e0564fee6d649272d529d4fa0ed278a31882
Oriol Fernandez Peña <oriol.fdez.penya@gmail.com>
2024-08-18 12:12:44 +0200

    Improvement on distances

--- higherdimknn/distances/distances.py
+++ higherdimknn/distances/distances.py
@@ -5,8 +5,8 @@ def minkowski_dist(x: np.ndarray, y: np.ndarray, p: float):
     if p <= 0:
         return ValueError("p must be > 0")
     
-    _dif = np.abs(x.flatten() - y.flatten())
-    return np.power(np.power(_dif, p).sum(), 1/p)
+    _dif = np.abs(x - y)
+    return np.power(np.power(_dif, p).sum(), 1 / p)
 
 def euclidean_dist(x: np.ndarray, y: np.ndarray):
     return minkowski_dist(x, y, 2)
